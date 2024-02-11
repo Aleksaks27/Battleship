8b94eeff3d187af0f79906db78dda3b466f43563
aleksaks27 <aleksaks27@gmail.com>
2024-02-11 01:21:30 +0000

    Add a game interface plus a menu screen for layout selection at the start. Fix bugs and add explanatory comments

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const playerBoard = document.querySelector("#player-board");
 const computerBoard = document.querySelector("#computer-board");
 const message = document.querySelector("#message");
+const restart = document.querySelector("#restart");
 
 const availableShips = [
     {name: "Carrier", length: 5},
@@ -9,6 +10,96 @@ const availableShips = [
     {name: "Submarine", length: 3},
     {name: "Patrol Boat", length: 2},
 ]
+const popup = document.querySelector("#popup");
+const startingBoard = document.querySelector("#starting-board");
+const randomise = document.querySelector("#randomise");
+const begin = document.querySelector("#begin");
+let introMessage = document.querySelector("#intro-message");
+let orientation = document.querySelector("#orientation");
+let index = 0;
+let randomLayout = false;
+
+popup.showModal();
+// The user can choose their starting layout on the popup menu. Much like boardSetup() below, the
+// position of a ship is only valid if it neither spills off the board nor crosses another ship.
+// The user will cycle through the fleet's available ships, each accompanied by a different message.
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        const square = document.createElement("div");
+        startingBoard.appendChild(square);
+        square.addEventListener("click", () => {
+            if (index < 5) {
+                if (orientation.textContent === "Horizontal") {
+                    if (j + availableShips[index].length - 1 < 10) {
+                        let k = availableShips[index].length - 1;
+                        let start = Number(String(i) + String(j));
+                        let valid = true;
+                        for (let z = start; z <= start + k; z++) {
+                            if (startingBoard.children[z].classList.contains("choice")) {
+                                valid = false;
+                            }
+                        }
+                        if (valid) {
+                            while (k >= 0) {
+                                startingBoard.children[start + k].classList.add("choice");
+                                k--;
+                            }
+                            let ship = availableShips[index];
+                            player.board.placeShip(ship.name, ship.length, [i, j],"Horizontal");
+                            index++;
+                            if (index < 5) introMessage.textContent = `Place your ${availableShips[index].name}`;
+                        }
+                    }
+                }
+                else {
+                    if (i + availableShips[index].length - 1 < 10) {
+                        let k = 10 * (availableShips[index].length - 1);
+                        let start = Number(String(i) + String(j));
+                        let valid = true;
+                        for (let z = start; z <= start + k; z += 10) {
+                            if (startingBoard.children[z].classList.contains("choice")) {
+                                valid = false;
+                            }
+                        }
+                        if (valid) {
+                            while (k >= 0) {
+                                startingBoard.children[start + k].classList.add("choice");
+                                k -= 10;
+                            }
+                            let ship = availableShips[index];
+                            player.board.placeShip(ship.name, ship.length, [i, j],"Verical");
+                            index++;
+                            if (index < 5) introMessage.textContent = `Place your ${availableShips[index].name}`;
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
+
+orientation.addEventListener("click", () => {
+    if (orientation.textContent === "Horizontal") orientation.textContent = "Vertical";
+    else orientation.textContent = "Horizontal";
+})
+
+randomise.addEventListener("click", () => {
+    randomLayout = true;
+    popup.close();
+    initialize();
+})
+
+begin.addEventListener("click", () => {
+    if (index === 5) {
+        console.log("test");
+        popup.close();
+        initialize();
+    }
+})
+
+restart.addEventListener("click", () => {
+    location.reload();
+})
 
 class Ship {
     constructor(name, length, start, orientation) {
@@ -20,10 +111,10 @@ class Ship {
         this.hits = 0;
         this.sunk = false;
     }
-
+    // The cover() function calculates the positions a ship occupies depending on its orientation.
     cover() {
         let positions = [];
-        if (this.orientation === "horizontal") {
+        if (this.orientation === "Horizontal") {
             let row = this.start[0];
             let i = this.start[1] + (this.length - 1);
             while (i >= this.start[1]) {
@@ -46,11 +137,13 @@ class Ship {
         this.hits++;
         this.isSunk();
     }
-
+    // The success message changes depending on which fleet the ship belongs to.
     isSunk() {
         if (this.hits === this.length) {
             this.sunk = true;
-            message.textContent = `The ${this.name} has sunk!`;
+            if (player.board.fleet.includes(this)) message.textContent = `Ỳour ${this.name} has sunk!`;
+            else message.textContent = `The computer's ${this.name} has sunk!`;
+            
         }
     }
 }
@@ -87,8 +180,8 @@ class Gameboard {
     }
 
     receiveAttack(coordinates) {
-        let row = coordinates[0];
-        let col = coordinates[1];
+        const row = coordinates[0];
+        const col = coordinates[1];
         if (this.board[row][col] === 0) {
             this.board[row][col] = "M";
         }
@@ -114,18 +207,20 @@ class Player {
         this.name = name;
         this.board = new Gameboard;
     }
-
+    // The function below chooses a ship's orientation randomly. After that, random coordinates
+    // are chosen. However that position is only valid if it does not spill off the board or
+    // overlap with another ship. The DOM is then updated accordingly.
     boardSetup() {
         for (let ship of availableShips) {
             let valid = false;
             let start = [];
             let orientation;
-            if (Math.random() > 0.5) orientation = "horizontal";
-            else orientation = "vertical";
+            if (Math.random() > 0.5) orientation = "Horizontal";
+            else orientation = "Vertical";
             while (!valid) {
                 start = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
                 let i;
-                if (orientation === "horizontal") {
+                if (orientation === "Horizontal") {
                     if ((start[1] + ship.length - 1) > 9) continue;
                     for (i = 0; i < ship.length; i++) {
                         if (this.board.board[start[0]][start[1] + i] === "S") break;
@@ -160,14 +255,16 @@ class Player {
             }
         }
     }
+    // When the computer plays a turn (with random coordinates), these will be rejected if already
+    // occupied by a hit or miss. In that case playTurn() recursively invokes until it finds a free
+    // spot.
 
     playTurn(coordinates, opponent) {
-        let row = coordinates[0];
-        let col = coordinates[1];
+        const row = coordinates[0];
+        const col = coordinates[1];
         if (opponent.board.board[row][col] === "H" || opponent.board.board[row][col] === "M") {
-            let x = Math.floor(Math.random() * 10);
-            let y = Math.floor(Math.random() * 10);
-            console.log([x, y]);
+            const x = Math.floor(Math.random() * 10);
+            const y = Math.floor(Math.random() * 10);
             return this.playTurn([x, y], opponent);
         }
         else {
@@ -186,10 +283,12 @@ class Player {
         }
     }
 }
-
+// The HTML boards/grids are created below. For the player's board, event listeners are added to
+// each unoccupied square for the user to play a round. These are only accessible while the game is
+// in progress. CSS properties of squares change accordingly.
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-        let square = document.createElement("div");
+        const square = document.createElement("div");
         square.classList.add("water");
         square.id = `${i}` + `${j}` + "p";
         playerBoard.appendChild(square);
@@ -198,7 +297,7 @@ for (let i = 0; i < 10; i++) {
 
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-        let square = document.createElement("div");
+        const square = document.createElement("div");
         square.classList.add("water");
         square.id = `${i}` + `${j}` + "c";
         computerBoard.appendChild(square);
@@ -221,17 +320,51 @@ for (let i = 0; i < 10; i++) {
         })
     }
 }
-
+// New player objects are created here. If the user has chosen a random layout, the board and fleet
+// are first erased to ensure no repetition. Otherwise, the layout chosen at the beginning is 
+// applied to the player's board.
 let computer = new Player("computer");
 let player = new Player("player");
-computer.boardSetup();
-player.boardSetup();
-
+function initialize() {
+    computer.boardSetup();
+    if (randomLayout) {
+        player.board.fleet = [];
+        player.board.board = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
+        player.boardSetup();
+    }
+    else {
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (startingBoard.children[Number(String(i) + String(j))].classList.contains("choice")) {
+                    player.board.board[i][j] = "S";
+                    playerBoard.children[Number(String(i) + String(j))].classList.add("ship");
+                }
+                else {
+                    player.board.board[i][j] = 0;
+                    playerBoard.children[Number(String(i) + String(j))].classList.add("water");
+                }
+            }
+        }
+    }
+}
+// Each time the user clicks a square on the computer's board, both play one turn until a winner
+// is crowned.
 function playGame(player, computer, guess) {
     if (!player.board.gameOver && !computer.board.gameOver) {
         player.playTurn(guess, computer);
-        let x = Math.floor(Math.random() * 10);
-        let y = Math.floor(Math.random() * 10);
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
         if (!computer.board.gameOver) {
             computer.playTurn([x, y], player);
         }
